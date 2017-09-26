c78f121d2364c26bf05f1851d0de9aabee195029
Dmitry Schekotov <dark.knight.9111@gmail.com>
2017-09-26 20:15:30 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const imagemin = require('imagemin');
+const imagemin_pngquant = require('imagemin-pngquant');
+const imagemin_mozjpeg = require('imagemin-mozjpeg');
+const imagemin_svgo = require('imagemin-svgo');
+
+module.exports = (buf) => {
+    return imagemin.buffer(buf, {
+        plugins: [
+            imagemin_mozjpeg({
+                quality: 90,
+                progressive: true,
+            }),
+            imagemin_pngquant({}),
+            imagemin_svgo({
+                plugins: [
+                    {removeViewBox: false}
+                ]
+            })
+        ]
+    })
+};
